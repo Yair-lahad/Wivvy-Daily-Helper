81370162d55b2e0262755ff44e3cabf1ab11cfb6
Yair-lahad <lahadya@gmail.com>
2025-07-15 09:36:01 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './wivvy-bird-librarian.png';
 import './App.css';
 
 function App() {
@@ -6,8 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Wivvy</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Weave your day with clarity
         </p>
         <a
           className="App-link"
